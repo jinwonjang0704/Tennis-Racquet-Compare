03146e45b794a670e4b2e7a99e967b9372f8b904
Jinwon Jang <jinwonjang@utexas.edu>
2020-08-28 17:07:04 -0500

    fixing reload issue

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ const racquetsRouter = require('./routes/racquets');
 
 app.use('/racquets', racquetsRouter);
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
